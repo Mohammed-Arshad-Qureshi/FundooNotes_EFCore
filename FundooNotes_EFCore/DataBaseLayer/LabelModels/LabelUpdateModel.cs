403a2681034dee6037b3377e305e93a0097e1321
Arshad <mohammedarshad183@gmail.com>
2022-07-21 20:37:40 +0530

    Added: Delete Api and refactored: updated Api

---
+++ FundooNotes_EFCore/DataBaseLayer/LabelModels/LabelUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBaseLayer.LabelModels
+{
+    public class LabelUpdateModel
+    {
+        public int UserId { get; set; }
+
+        public int NoteId { get; set; }
+
+        public string LabelName { get; set; }
+    }
+}
--- FundooNotes_EFCore/RepositoryLayer/Services/LabelRL.cs
+++ FundooNotes_EFCore/RepositoryLayer/Services/LabelRL.cs
@@ -71,27 +71,27 @@ namespace RepositoryLayer.Services
             }
         }
 
-        public async Task<List<LabelModel>> GetLabelByNoteId(int UserId,int NoteId)
+        public async Task<List<LabelModel>> GetLabelByNoteId(int UserId, int NoteId)
         {
             try
             {
                 var label = this.fundooContext.Label.FirstOrDefault(x => x.UserId == UserId);
                 var result = await (from user in fundooContext.Users
-                                   join notes in fundooContext.Notes on user.UserId equals UserId //where notes.NoteId == NoteId
-                                   join labels in fundooContext.Label on notes.NoteId equals labels.NoteId
-                                   where labels.NoteId == NoteId
-                                   select new LabelModel
-                                   {
-                                       LabelId = labels.LabelId,
-                                       UserId = UserId,
-                                       NoteId = notes.NoteId,
-                                       Title = notes.Title,
-                                       FirstName = user.FirstName,
-                                       LastName = user.LastName,
-                                       Email = user.Email,
-                                       Description = notes.Description,
-                                       LabelName = labels.LabelName,
-                                   }).ToListAsync();
+                                    join notes in fundooContext.Notes on user.UserId equals UserId //where notes.NoteId == NoteId
+                                    join labels in fundooContext.Label on notes.NoteId equals labels.NoteId
+                                    where labels.NoteId == NoteId && labels.UserId == UserId
+                                    select new LabelModel
+                                    {
+                                        LabelId = labels.LabelId,
+                                        UserId = UserId,
+                                        NoteId = notes.NoteId,
+                                        Title = notes.Title,
+                                        FirstName = user.FirstName,
+                                        LastName = user.LastName,
+                                        Email = user.Email,
+                                        Description = notes.Description,
+                                        LabelName = labels.LabelName,
+                                    }).ToListAsync();
                 return result;
 
             }
@@ -102,14 +102,35 @@ namespace RepositoryLayer.Services
             }
         }
 
-        public async Task<bool> UpdateLable(int NoteId,string Labelname)
+        public async Task<bool> UpdateLable(int UserId, int LabelId, string Labelname)
         {
             try
             {
-                var label = this.fundooContext.Label.FirstOrDefault(x => x.NoteId == NoteId && x.LabelName != Labelname);
-                if (label != null)
+                var label = this.fundooContext.Label.FirstOrDefault(x => x.LabelId == LabelId && x.UserId == UserId);
+                var check = this.fundooContext.Label.FirstOrDefault(x => x.NoteId == label.NoteId && x.LabelName == Labelname);
+                if (label != null && check == null)
                 {
                     label.LabelName = Labelname;
+                    this.fundooContext.SaveChanges();
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<bool> DeleteLabel(int UserId, int LabelId)
+        {
+            try
+            {
+                var label = this.fundooContext.Label.FirstOrDefault(x => x.LabelId == LabelId && x.UserId == UserId);
+                if (label != null)
+                {
+                    this.fundooContext.Remove(label);
                     this.fundooContext.SaveChanges();
                     return true;
                 }
